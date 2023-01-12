338da1b77547f568caa372b13699fba90cc3b43e
mannig1224 <egatica51@gmail.com>
2023-01-12 02:26:48 -0700

    getting started

---
+++ pages/events/[...slug].js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FilteredEventsPage() {
+  return (
+    <div>
+      <h1>This is the filtered event page</h1>
+    </div>
+  );
+}
+
+export default FilteredEventsPage;
